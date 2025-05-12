01d4a622a8dd328e9e36967b28b98a94b4df452c
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-05-12 19:28:49 -0300

    updated info card tsx

--- src/app/components/InfoCard.tsx
+++ src/app/components/InfoCard.tsx
@@ -1,26 +1,53 @@
 import React from "react";
 import { features } from "../constants/Features";
+import Button from "./Button";
 
 export default function InfoCard() {
+  interface InfoCardContent {
+    title: string;
+    description: string;
+  }
+
+  const infoCardContents: InfoCardContent[] = [
+    {
+      title:
+        "Grow your business with the Fieldguide AI Platform for Advisory and Audit Firms",
+      description:
+        "Fieldguide increases margins, revenue, and client satisfaction across all advisory and audit engagements.",
+    },
+  ];
+
   return (
     <div className="flex-1 space-y-6">
-      <p className="text-sm uppercase text-primary font-medium">
-        Why FieldGuide
-      </p>
-      <h3 className="text-2xl font-semibold">Smarter Audits, Happier Teams</h3>
-      <p className=" mb-8">
-        From planning to delivery, our platform simplifies every step of the
-        audit process.
-      </p>
+      {infoCardContents.map((content, index) => (
+        <div key={index}>
+          <p className="text-sm uppercase text-primary font-medium mb-4">
+            PRODUCTS
+          </p>
+          <h3 className="text-[32px] leading-[38px] font-semibold mb-4">
+            {content.title}
+          </h3>
+          <p className="text-[16px] leading-[22px] font-normal text-gray-white2">
+            {content.description}
+          </p>
+        </div>
+      ))}
 
       <div className="grid grid-cols-2 gap-6">
         {features.map((feature, index) => (
           <div key={index}>
-            <p className="font-semibold text-lg">{feature.title}</p>
-            <p className="text-sm">{feature.description}</p>
+            <p className="text-[16px] leading-[22px] font-medium">
+              {feature.title}
+            </p>
+
+            <p className="text-[14px] leading-[22px] font-normal text-gray-white2">
+              {feature.description}
+            </p>
           </div>
         ))}
       </div>
+
+      <Button text="Explore Products" />
     </div>
   );
 }
