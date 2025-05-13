122fb9999f62c10671093fc88af3fdcc147bec00
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-05-13 17:42:27 -0300

    updated testimonial card

--- src/app/components/TestimonialCard.tsx
+++ src/app/components/TestimonialCard.tsx
@@ -2,22 +2,43 @@ import React from "react";
 import Image from "next/image";
 
 export default function TestimonialCard() {
+  interface TestimonialCardInfo {
+    image: string;
+    alt: string;
+    testimonial: string;
+    userName: string;
+    role: string;
+  }
+
+  const testimonialCardInfo: TestimonialCardInfo = {
+    image: "/images/assets/man1.svg",
+    alt: "John Doe",
+    testimonial:
+      " “Software for accounting firms is notoriously outdated, slow, and hard to use. Fieldguide is different – itʼs a more streamlined, cloud-first approach.” ",
+    userName: "John Doe",
+    role: "Partner at Aprio",
+  };
+
   return (
     <div className="flex-1 lg:flex-[0.7] shadow-lg rounded-xl overflow-hidden flex flex-col md:flex-row">
       <Image
-        src="/images/assets/man1.svg"
-        alt="Testimonial"
-        width={180}
-        height={180}
-        className="w-full md:w-48 h-full object-cover"
+        src={testimonialCardInfo.image}
+        alt={testimonialCardInfo.alt}
+        width={200}
+        height={260}
+        className="w-full max-w-[200px] h-auto object-cover rounded-lg"
       />
       <div className="p-6 flex flex-col justify-center">
-        <p className="text-lg font-medium mb-4 italic">
-          “Using this platform has revolutionized how we handle audits. The
-          collaboration and automation saved us countless hours.”
+        <p className="text-[24px] leading-[32px] font-medium mb-4 italic">
+          {testimonialCardInfo.testimonial}
         </p>
-        <p className="font-semibold">Jane Doe</p>
-        <p className="text-sm">Partner at Aprio</p>
+        <p className="text-[16px] leading-[22px] font-normal">
+          {testimonialCardInfo.userName}
+        </p>
+
+        <p className="text-[16px] leading-[22px] font-normal text-gray-white2">
+          {testimonialCardInfo.role}
+        </p>
       </div>
     </div>
   );
