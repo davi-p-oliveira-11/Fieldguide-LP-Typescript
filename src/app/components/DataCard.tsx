430363a2f80cd1be7a2b127bf77f6ea638649b95
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-05-13 18:18:22 -0300

    updated data card

--- src/app/components/DataCard.tsx
+++ src/app/components/DataCard.tsx
@@ -1,11 +1,29 @@
 import React from "react";
 
 export default function DataCard() {
+  interface StatsCard {
+    multipleNum: string;
+    subtext: string;
+    text: string;
+  }
+
+  const statsCardInfo: StatsCard = {
+    multipleNum: "5X",
+    subtext: "Business Growth",
+    text: "Fieldguide innovations and efficiencies helped Maxwell Locke & Ritter grow their practice by",
+  };
+
   return (
-    <div className="flex-1 lg:flex-[0.3] bg-gray-50 shadow-md rounded-xl p-8 text-center flex flex-col justify-center">
-      <h2 className="text-4xl font-bold text-blue-600 mb-2">98%</h2>
-      <p className="text-lg font-semibold mb-1">Client Satisfaction</p>
-      <p className="text-sm ">Based on survey responses from over 200 firms</p>
+    <div className="flex-1 lg:flex-[0.3] bg-gray-50 shadow-md rounded-xl p-8 flex flex-col justify-center text-left mb-1">
+      <h2 className="text-[120px] leading-[96px] font-semibold text-blue-600 mb-2">
+        {statsCardInfo.multipleNum}
+      </h2>
+      <p className="text-[16px] leading-[22px] font-medium mb-4">
+        {statsCardInfo.subtext}
+      </p>
+      <p className="text-[16px] leading-[22px] font-normal text-gray-white2">
+        {statsCardInfo.text}
+      </p>
     </div>
   );
 }
